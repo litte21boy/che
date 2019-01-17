abd2da9e0b284c245bea92aff7a241e8618d55c9
Your Name <you@example.com>
2019-01-17 19:49:04 +0800

    添加车辆删除功能

--- main.c
+++ main.c
@@ -1,16 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-struct point {
+struct point
+{
     int x;
     int y;
 };
 int main()
 {
-    int index=0;
+    int index = 0;
     struct point arr[100];
     printf("huanyingjinruzhuanchangcheliangguanlixitong\n");
-    while(1){
+    while (1)
+    {
         printf("1---tianjiacheliangweizhi\n");
         printf("2---shanchucheliangweizhi\n");
         printf("3---xianshisuoyoucheliangweizhi\n");
@@ -19,37 +21,53 @@ int main()
 
         printf("qingshuruxiangyingdegongnengbianhao\n");
         int code;
-        scanf("%d",&code);
-        if(code==1){
+        scanf("%d", &code);
+        if (code == 1)
+        {
             printf("qingshuruniyaotianjiadexzuobiaoheyzuobiao\n");
             int x;
             int y;
-            scanf("%d",&x);
-            scanf("%d",&y);
+            scanf("%d", &x);
+            scanf("%d", &y);
 
-            arr[index].x=x;
-            arr[index].y=y;
+            arr[index].x = x;
+            arr[index].y = y;
 
             index++;
             printf("tianjiachenggong,dianjihuichejixu\n");
             char xx;
-            scanf("%c",&xx);
-            scanf("%c",&xx);
+            scanf("%c", &xx);
+            scanf("%c", &xx);
+        }
+        if (code == 2)
+        {
+            printf("shanchu\n");
+            if (index > 0)
+            {
+                index--;
+                printf("shanchuchenggong,dianjihuichejixu\n");
+                char xx;
+                scanf("%c", &xx);
+                scanf("%c", &xx);
+            }
+            else{
+                printf("wutingchewei,shanchushibai,dianjihuichejixu\n");
+                char xx;
+                scanf("%c", &xx);
+                scanf("%c", &xx);
 
+            }
         }
-        if(code==2){
-            
+        if (code == 3)
+        {
         }
-        if(code==3){
-            
+        if (code == 4)
+        {
         }
-        if(code==4){
-            
-        }
-        if(code==5){
+        if (code == 5)
+        {
             printf("tuichuxitong\n");
             break;
-            
         }
     }
 }
