cba44586088314085bc477a2a210f6cadd5a38a6
Your Name <you@example.com>
2019-01-17 20:09:59 +0800

    添加车辆位置距离，用户最近的距离

--- main.c
+++ main.c
@@ -65,13 +65,42 @@ int main()
             {
                 printf("di%dgecheliangdezuobiaoweizhishi(%d,%d)\n", i + 1, arr[i].x, arr[i].y);
             }
-            printf("dianjihuichejixu\n" );
+            printf("dianjihuichejixu\n");
             char xx;
             scanf("%c", &xx);
             scanf("%c", &xx);
         }
         if (code == 4)
         {
+            printf("qingshurunidangqianweizhizuobiao:\n");
+            int x;
+            int y;
+            scanf("%d", &x);
+            scanf("%d", &y);
+            //用当前的x和y坐标，与每一个停车位进行计算，然后将最小的值输出
+            //两点之间的距离公式
+            double min = 100000;
+            int carID = 0;
+
+            for (int i = 0; i < index; i++)
+            {
+                int dx = x - arr[i].x;
+                int dy = y - arr[i].y;
+                double l = sqrt(dx * dx + dy * dy);
+
+                printf("yonghudangqianweizhiyu%dgecheliangdejulishi%lf\n", i + 1, l);
+                if (min > l)
+                {
+                    min = l;
+                    carID = i;
+                }
+            }
+            printf("yonghudangqianweizhiyudi%dgechedejuliwei,%lf,shizuixiaojuli\n", carID + 1, min);
+
+            printf("dianjihuichejixu\n");
+            char xx;
+            scanf("%c", &xx);
+            scanf("%c", &xx);
         }
         if (code == 5)
         {
